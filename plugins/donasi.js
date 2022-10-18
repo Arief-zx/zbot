4c9954ce3e9045c3375c06d7e48e274a9d12166b
Arief-zx <77441608+Arief-zx@users.noreply.github.com>
2022-10-19 01:20:26 +0700

    donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -6,7 +6,7 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let str = `${global.wm}
 
 ┌─「 Donasi • Pulsa 」
-│ • *Telkomsel:* [${global.ppulsa}]
+│ • *Three:* [${global.ppulsa}]
 ❏────
 
 ┌─「 Donasi • Non Pulsa 」
@@ -18,10 +18,10 @@ let str = `${global.wm}
 let wibu = `https://i.ibb.co/P5BBzbf/286637238-392674839545088-948545114162206238-n.jpg` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Sewa Bot','.sewa', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
+    mediaUrl: "https://instagram.com/arieff_westa",
     mediaType: "VIDEO",
-    description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'WH-MODS-DEV-V1 MultiDevice',
+    description: "https://www.instagram.com/p/ChtqnwKLaum/?utm_source=ig_web_copy_link", 
+    title: 'ZXCODE BOT MultiDevice',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sig
