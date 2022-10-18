f1dd01327ac419a0d56d89b69b2243a69b0a2fd4
Arief-zx <77441608+Arief-zx@users.noreply.github.com>
2022-10-19 01:35:26 +0700

    ppcouple.js

--- plugins/ppcouple.js
+++ plugins/ppcouple.js
@@ -5,8 +5,8 @@ let handler = async (m, { conn }) => {
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.status) throw json
-  await conn.sendFile(m.chat, json.result.male, '', 'Nih PP Yg Cowo', m)
-  await conn.sendFile(m.chat, json.result.female, '', 'Dan Ni PP YgCewe', m)
+  await conn.sendFile(m.chat, json.result.male, '', 'Male', m)
+  await conn.sendFile(m.chat, json.result.female, '', 'Female', m)
 }
 handler.help = ['ppcouple', 'ppcp']
 handler.tags = ['internet', 'limitmenu']
